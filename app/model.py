87b043e212b4308b568d59c1be3b739a2d443f8e
Neetu Pundir <neetu.pundir@outlook.com>
2024-12-01 13:31:00 +0000

    best_model.path added

--- app/model.py
+++ app/model.py
@@ -1,13 +1,15 @@
 import os
 import wandb
-from loadotenv import load_env # to load env variable
+from loadotenv import load_env
+from pathlib import Path
 
 
-load_env(file_loc='/workspaces/fruit-classifier-mlops-class/.env')
+load_env()
 wandb_api_key = os.environ.get('WANDB_API_KEY')
 print(wandb_api_key)
 
 MODELS_DIR = 'models'
+MODEL_FILE_NAME = 'best_model.pth'
 
 os.makedirs(MODELS_DIR, exist_ok=True)
 
@@ -18,10 +20,17 @@ def download_artifact():
     wandb_project = os.environ.get('WANDB_PROJECT')
     wandb_model_name = os.environ.get('WANDB_MODEL_NAME')
     wandb_model_version = os.environ.get('WANDB_MODEL_VERSION')
+    wandb_full_path = os.environ.get('FULL_MODEL_PATH') # this is just for debugging
 
-    artifact_path = f'{wandb_org}/{wandb_project}/{wandb_model_name}:{wandb_model_version}'
+    # This one might work (?)
+    #artifact_path = str(Path(wandb_org) / wandb_project / wandb_model_name) + (f':{wandb_model_version}')
+    print(wandb_full_path)
+    artifact_path = wandb_full_path
+    
+    wandb.login()
+    artifact = wandb.Api().artifact(artifact_path, type='model')
+    artifact.download(root=MODELS_DIR)
 
     print(artifact_path)
 
-download_artifact() 
-    
+download_artifact()
